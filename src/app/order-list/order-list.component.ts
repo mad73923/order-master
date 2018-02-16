d2651954485307fb723654d8d15e8cb498bbc86c
Matthias Weis <matthias1.weis@web.de>
2018-02-16 23:01:57 +0100

    order sum, move functions to order class, right align

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -12,16 +12,28 @@ export class OrderListComponent implements OnInit {
 
   order = mockOrder;
 
+  order_sum : number;
+
   deleteItem(item: OrderItem){
     let index = this.order.items.indexOf(item, 0);
     if(index > -1){
       this.order.items.splice(index, 1);
     }
+    this.update_order_sum();
+  }
+
+  update_order_sum(){
+    let erg = 0;
+    this.order.items.forEach(element => {
+      erg += element.price;
+    });
+    this.order_sum = erg;
   }
 
   constructor() { }
 
   ngOnInit() {
+    this.update_order_sum();
   }
 
 }
