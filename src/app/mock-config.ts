783aecae28595056b68170e341f1ec0abd2dfa9b
Matthias Weis <matthias1.weis@web.de>
2018-02-17 09:18:23 +0100

    added container, mock-config, pizza class

---
+++ src/app/mock-config.ts
@@ -0,0 +1,27 @@
+import {Pizza} from './pizza'
+
+export const mockConfig = {
+    availableItems: [
+        {
+            name: "Magaritha",
+            price: 3.5,
+            ingredients: [
+                "Tomatensauce", "Käse"
+            ]
+        },
+        {
+            name: "Funghi",
+            price: 4,
+            ingredients: [
+                "Tomatensauce", "Käse", "Pilze"
+            ]
+        },
+        {
+            name: "Proscuitto",
+            price: 4.5,
+            ingredients: [
+                "Tomatensauce", "Käse", "Schinken"
+            ]
+        }
+    ]
+};
---
+++ src/app/pizza.ts
@@ -0,0 +1,5 @@
+export class Pizza{
+    name: string;
+    ingredients: string[];
+    price: number;
+}
