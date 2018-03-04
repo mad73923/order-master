637297119a47aa8c7786e758b86ecd68bacdc5cf
Matthias Weis <matthias1.weis@web.de>
2018-03-04 11:45:26 +0100

    order service still not 100%

--- src/app/order.service.spec.ts
+++ src/app/order.service.spec.ts
@@ -4,14 +4,20 @@ import { OrderService } from './order.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/empty';
 
 class FakeHttpClientModule {
   post(): Observable<any> {
     return  Observable.of('test');
   }
 
-  get() {
-    return {catch: ()=> {}};
+  get(): Observable<any> {
+    return Observable.throw('');
+  }
+
+  put(): Observable<any>{
+    return Observable.of();
   }
 }
 
@@ -27,6 +33,13 @@ describe('OrderService', () => {
   }));
   it('call members TODO error handling', inject([OrderService], (service: OrderService) => {
     service.get_Active_Orders();
+  }));
+
+  it('should create new order', inject([OrderService], (service: OrderService) => {
     service.new_Order({id: 123, date: 6462, items: [], paid: false});
+  }));
+
+  it('should create new order', inject([OrderService], (service: OrderService) => {
+    service.update_item({_id: "sjdfj", name: "pommes", price: 3.6, count: 1, stages: []});
   }));
 });
