41d5137cff31e19529dc709dbc8c17041922ecfe
Matthias Weis <matthias1.weis@web.de>
2018-03-04 10:37:50 +0100

    no errors

--- src/app/order.service.spec.ts
+++ src/app/order.service.spec.ts
@@ -11,6 +11,7 @@ class FakeHttpClientModule {
   }
 
   get() {
+    return {catch: ()=> {}};
   }
 }
 
--- src/app/stage/stage.component.spec.ts
+++ src/app/stage/stage.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StageComponent } from './stage.component';
+import { OrderService } from '../order.service';
+import { Order } from '../../shared/order';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+
+class FakeService {
+  new_Order(order: Order) {
+
+  }
+
+  get_Active_Orders(): Observable<Order[]> {
+    return Observable.of();
+  }
+}
 
 describe('StageComponent', () => {
   let component: StageComponent;
   let fixture: ComponentFixture<StageComponent>;
 
   beforeEach(async(() => {
+    TestBed.overrideComponent(StageComponent, {set: {providers: [{provide: OrderService, useClass: FakeService}]}});
     TestBed.configureTestingModule({
-      declarations: [ StageComponent ]
+      declarations: [ StageComponent ],
+      providers: [OrderService]
     })
     .compileComponents();
   }));
